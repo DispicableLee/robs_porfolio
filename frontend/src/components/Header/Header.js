c8626f31466a59e8a732d77a18224bfd4e6ae466
DispicableLee <lee.robert053@gmail.com>
2024-05-08 13:23:27 -0400

    nav links

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,15 +1,27 @@
 import React from "react";
+import githubIcon from '../../icons/githubicon.png'
+import linkedInIcon from '../../icons/linkedinicon.png'
 import "./Header.css"
 
 export default function AppHeader(){
 
 
     const headerNavLinks = [
-        {
-            icon: "",
-            url: "https://www.instagram.com/hawtcurryans/",
-            userName: "@hawtcurryans"
+        // {
+        //     icon: "",
+        //     url: "https://www.instagram.com/hawtcurryans/",
+        //     username: "@hawtcurryans"
 
+        // },
+        {
+            icon: githubIcon,
+            url: "https://github.com/DispicableLee",
+            username: "DispicableLee"
+        },
+        {
+            icon: linkedInIcon,
+            url: "https://www.linkedin.com/in/robert-lee-webdeveloper/",
+            username: "Robert Lee"
         }
     ]
 
@@ -17,6 +29,16 @@ export default function AppHeader(){
     return (
         <div id="header-main">
             <h1 className="header-main-header">Robert Lee</h1>
+
+            <div className="nav-links-container">
+                <div className="nav-link">
+                    <a href="https://www.linkedin.com/in/robert-lee-webdeveloper/"
+                        target="_blank"
+                    >
+                        <img src={linkedInIcon}/>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
